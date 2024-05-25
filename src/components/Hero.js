6f0270f68eaf9173f58af1ce686f2ddb725cedbd
lotaxgizmo <lotaoko2@gmail.com>
2024-05-25 23:33:21 +0100

    dd

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,22 @@
-import React from 'react' 
+import React from 'react'
 import herotext from "../assets/herotext.svg"
+import herotextmobile from "../assets/herotextmobile.svg"
+import star from "../assets/star.svg"
 
 function Hero() {
   return (
     <div className='flex flex-col justify-center items-center lg:my-14 mt-14 mb-14 p-4 lg:p-0 relative '>
-        <img src={herotext} alt="" className='' />
-        <p className='text-xl text-center text-white font-light dw-72 lg:mb-322 flex lg:hidden'>
-                        Revolutionalize your crypto Trading with our Defi Swap Platform High Value Locked
-                    </p> 
-        <p className=' text-xl text-right text-white font-light w-72 lg:mb-322 lg:flex hidden absolute self-start left-[20%]'>
-                        Revolutionalize your crypto Trading with our Defi Swap Platform High Value Locked
-                    </p> 
+      <img src={herotext} alt="" className='lg:hidden flex' />
+      <div className="hidden lg:flex relative">
+      <img src={herotextmobile} alt="" className='' />
+      <img src={star} alt="" className='absolute -left-36 spin' />
+      </div>
+      <p className='text-xl text-center text-white font-light dw-72 lg:mb-322 flex lg:hidden'>
+        Revolutionalize your crypto Trading with our Defi Swap Platform High Value Locked
+      </p>
+      <p className=' text-xl text-right text-white font-light w-72 lg:mb-322 lg:flex hidden absolute self-start left-[15%]'>
+        Revolutionalize your crypto Trading with our Defi Swap Platform High Value Locked
+      </p>
     </div>
   )
 }
